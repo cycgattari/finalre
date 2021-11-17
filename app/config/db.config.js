764bedece2ff3122cfe948beb6b7b9fb7ab0a20a
Maxi Gattari <cycgattari@hotmail.com>
2021-11-17 18:51:36 -0300

    dfg

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,5 +2,12 @@ module.exports = {
   HOST: "us-cdbr-east-04.cleardb.com",
   USER: "b996cf1fdc4d11",
   PASSWORD: "d06fcaaa ",
-  DB: "heroku_dd20ed31fda6079"
+  DB: "heroku_dd20ed31fda6079",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 };
--- server.js
+++ server.js
@@ -11,17 +11,25 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
-app.use(express.json()); /* bodyParser.json() is deprecated */
+app.use(express.json());  /* bodyParser.json() is deprecated */
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
+app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
+
+const db = require("./app/models");
+
+db.sequelize.sync();
+// // drop the table if it already exists
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
 
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
-require("./app/routes/tutorial.routes.js")(app);
+require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
