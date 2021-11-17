27f1419c8de9e9bc418e27f2e1d17e015c5c29bf
Maxi Gattari <cycgattari@hotmail.com>
2021-11-17 19:23:54 -0300

    sdf

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: "us-cdbr-east-04.cleardb.com",
   USER: "b996cf1fdc4d11",
-  PASSWORD: "d06fcaaa ",
+  PASSWORD: "d06fcaaa",
   DB: "heroku_dd20ed31fda6079",
   dialect: "mysql",
   pool: {
